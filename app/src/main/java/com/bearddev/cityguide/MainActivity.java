1a392cef0d6c425c964b60fd8b7125e93ae39fa9
Radosław Cięciwa <radekcieciwa@gmail.com>
2015-05-08 19:20:35 +0200

    - Functionality of adding fragments to stack.

--- app/src/main/java/com/bearddev/cityguide/MainActivity.java
+++ app/src/main/java/com/bearddev/cityguide/MainActivity.java
@@ -1,5 +1,8 @@
 package com.bearddev.cityguide;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -15,19 +18,36 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onBackPressed() {
+        // we will remove last fragment from stack
+        if (!removeFragmentIfStackIsLargerThanOne()) {
+            // if we didn't remove anything that means we want to close application
+            super.onBackPressed();
+        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
+    public boolean removeFragmentIfStackIsLargerThanOne() {
+        int stackEntryCount = getFragmentManager().getBackStackEntryCount();
+        if (stackEntryCount > 0) {
+            // get last back stack entry, to get last fragment on stack
+            FragmentManager.BackStackEntry stackEntry = getFragmentManager().getBackStackEntryAt(stackEntryCount - 1);
+            // find fragment in view with his name add with add method
+            Fragment f = getFragmentManager().findFragmentByTag(stackEntry.getName());
+            // remove fragment from stack
+            getFragmentManager().beginTransaction().remove(f).commit();
+            // remove information that fragment is in stack
+            getFragmentManager().popBackStack();
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        return false;
+    }
+
+    public void pushFragment(Fragment fragment, String name) {
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction ft = manager.beginTransaction();
+        ft.add(R.id.a_main_content, fragment, name);
+        ft.addToBackStack(name);
+        ft.commit();
     }
 }
